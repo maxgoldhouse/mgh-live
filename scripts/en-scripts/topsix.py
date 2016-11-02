a35371561fcc50854cbdc4a1dfecb7a39227ab62
dindinet <github.dklongley@neverbox.com>
2016-11-02 16:16:21 +0000

    front page offer in top six

--- scripts/en-scripts/topsix.py
+++ scripts/en-scripts/topsix.py
@@ -68,10 +68,10 @@ for item in topsixdict['props']:
     	print item[key]
 '''
 ### insert the accommodation offer into the position 1of the list
-# prop = {}
-# prop['proptype'] = 'mghoffer'
-# prop['offerHTML'] = '<div class="featured_property_wrap col-sm-4"><style>#offer{font-size:20px;} .mgh-offer{text-align:center;}</style><div class="mgh-offer"><img src="//lh3.googleusercontent.com/-GTT16P545Io/WA8sWvHiQZI/AAAAAAAAP6o/8rxFFVCigjwJ_o2_7QVO7SU-NCs_dFAHACL0B/s0-rj/150-euros.jpg" class="img-thumbnail img-circle img-responsive"><div id="offer">COULD WE PAY<br> YOUR HOTEL BILL?</div><div class="col-sm-offset-1"><a href="/mgh-offer.html#hotelpaid"><button class="btn btn-success">Tell me more</button></a></div></div></div>'
-# topsixdict['props'].insert(1,prop)
+prop = {}
+prop['proptype'] = 'mghoffer'
+prop['offerHTML'] = '<div class="featured_property_wrap col-sm-4"><style>#offer{font-size:20px;} .mgh-offer{text-align:center;}</style><div class="mgh-offer"><img src="//lh3.googleusercontent.com/-GTT16P545Io/WA8sWvHiQZI/AAAAAAAAP6o/8rxFFVCigjwJ_o2_7QVO7SU-NCs_dFAHACL0B/s0-rj/150-euros.jpg" class="img-thumbnail img-circle img-responsive"><div id="offer">COULD WE PAY<br> YOUR HOTEL BILL?</div><div class="col-sm-offset-1"><a href="/mgh-offer.html#hotelpaid"><button class="btn btn-success">Tell me more</button></a></div></div></div>'
+topsixdict['props'].insert(1,prop)
 
 outputText = template.render(topsixdict)
 #print outputText
