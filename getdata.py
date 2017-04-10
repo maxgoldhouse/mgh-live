11da915d8e3d96f09485631164a3f3add01fff8f
maxgoldhouse <dklongley@maxgoldhouse.com>
2017-04-10 14:17:49 +0100

    refsearch.html in topsixindex.jinja

--- getdata.py
+++ getdata.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python
 import urllib
 urllib.urlretrieve ("http://mgh-props.appspot.com/pydata", "_mgh_data.py")
+urllib.urlretrieve ("http://mgh-props.appspot.com/refs", "refsearch.html")
