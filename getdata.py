aaacb46a87f3d8e1c9b76edec42474a67f1f5ce6
maxgoldhouse <dklongley@maxgoldhouse.com>
2017-04-10 20:06:05 +0100

    reverting back to Friday

--- getdata.py
+++ getdata.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
 import urllib
 urllib.urlretrieve ("http://mgh-props.appspot.com/pydata", "_mgh_data.py")
+
 #urllib.urlretrieve ("http://mgh-props.appspot.com/refs", "refsearch.html")
