faa3ec0b2d81ddc0dd23ce9490ccf4a9645988ee
maxgoldhouse <dklongley@maxgoldhouse.com>
2019-11-24 23:55:16 +0000

    Update getdata.py

--- getdata.py
+++ getdata.py
@@ -1,4 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import urllib
-urllib.urlretrieve ("http://mgh-props.appspot.com/pydata", "_mgh_data.py")
-urllib.urlretrieve ("http://mgh-props.appspot.com/refs", "refsearch.html")
+import urllib
+
+testfile = urllib.URLopener()
+testfile.retrieve("http://mgh-props.appspot.com/pydata", "_mgh_data.py")
+testfile.retrieve("http://mgh-props.appspot.com/refs", "refsearch.html")
+#urllib.urlretrieve ("http://mgh-props.appspot.com/pydata", "_mgh_data.py")
+#urllib.urlretrieve ("http://mgh-props.appspot.com/refs", "refsearch.html")
