0de8a30e8623a4fe167557c507e1f02d50fc5854
maxgoldhouse <dklongley@maxgoldhouse.com>
2019-11-24 13:00:44 +0000

    [skip ci]

---
+++ scripts/NEW-en-scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+date
+sudo pip install Jinja2
+echo 'Fetching data from mgh-props'
+python getdata.py
+echo 'Data fetched'
+echo 'NEW ENGLISH'
+python ./scripts/NEW-en-scripts/topsix.py
+echo 'NEW topsix EN fin'
+python ./scripts/NEW-en-scripts/latest.py
+echo 'NEW latest EN fin'
+python ./scripts/NEW-en-scripts/proplist.py
+echo 'NEW proplist EN fin'
+python scripts/NEW-en-scripts/propdetail.py
+echo 'NEW propdetail EN fin'
+sudo pip uninstall Jinja2 --yes
+date
