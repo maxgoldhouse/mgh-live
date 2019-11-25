07c2c30af2f2a31a2df7cb4707825c2ffbbc09f5
maxgoldhouse <dklongley@maxgoldhouse.com>
2019-11-25 10:56:17 +0000

    Update getdata.py

--- getdata.py
+++ getdata.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-import urllib.request
+import urllib
 
-x = urllib.request.urlopen('http://mgh-props.appspot.com/pydata')
-saveFile = open('_mgh_data.py','w')
-saveFile.write(str(x.read()))
-saveFile.close()
+urllib.urlretrieve ("http://mgh-props.appspot.com/pydata", "_mgh_data.py")
+urllib.urlretrieve ("http://mgh-props.appspot.com/refs", "refsearch.html")
 
-y = urllib.request.urlopen('http://mgh-props.appspot.com/refs')
-saveFile = open('refsearch.html','w')
-saveFile.write(str(y.read()))
-saveFile.close()
-#urllib.urlretrieve ("http://mgh-props.appspot.com/pydata", "_mgh_data.py")
-#urllib.urlretrieve ("http://mgh-props.appspot.com/refs", "refsearch.html")
+#import urllib.request
+
+#x = urllib.request.urlopen('http://mgh-props.appspot.com/pydata')
+#saveFile = open('_mgh_data.py','w')
+#saveFile.write(str(x.read()))
+#saveFile.close()
+
+#y = urllib.request.urlopen('http://mgh-props.appspot.com/refs')
+#saveFile = open('refsearch.html','w')
+#saveFile.write(str(y.read()))
+#saveFile.close()
