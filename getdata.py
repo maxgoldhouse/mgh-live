f2374667c50e2d2e1e1859406c7326e40a358d75
maxgoldhouse <dklongley@maxgoldhouse.com>
2019-11-25 00:12:01 +0000

    Update getdata.py

--- getdata.py
+++ getdata.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 import urllib.request
 
+x = urllib.request.urlopen('http://mgh-props.appspot.com/pydata')
+saveFile = open('_mgh_data.py','w')
+saveFile.write(str(x.read()))
+saveFile.close()
 
-    x = urllib.request.urlopen('http://mgh-props.appspot.com/pydata')
-    saveFile = open('_mgh_data.py','w')
-    saveFile.write(str(x.read()))
-    saveFile.close()
-
-    y = urllib.request.urlopen('http://mgh-props.appspot.com/refs')
-    saveFile = open('refsearch.html','w')
-    saveFile.write(str(y.read()))
-    saveFile.close()
+y = urllib.request.urlopen('http://mgh-props.appspot.com/refs')
+saveFile = open('refsearch.html','w')
+saveFile.write(str(y.read()))
+saveFile.close()
 #urllib.urlretrieve ("http://mgh-props.appspot.com/pydata", "_mgh_data.py")
 #urllib.urlretrieve ("http://mgh-props.appspot.com/refs", "refsearch.html")
