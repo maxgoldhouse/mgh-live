2330ec149c1c8f33a0fb52d2f78d939e767c167e
maxgoldhouse <dklongley@maxgoldhouse.com>
2019-11-25 10:34:54 +0000

    Update deploy.sh

--- scripts/NEW-en-scripts/deploy.sh
+++ scripts/NEW-en-scripts/deploy.sh
@@ -10,6 +10,6 @@ python ./scripts/NEW-en-scripts/latest.py
 echo 'NEW latest EN fin'
 python ./scripts/NEW-en-scripts/proplist.py
 echo 'NEW proplist EN fin'
-#python scripts/NEW-en-scripts/propdetail.py
-#echo 'NEW propdetail EN fin'
+python scripts/NEW-en-scripts/propdetail.py
+echo 'NEW propdetail EN fin'
 date
