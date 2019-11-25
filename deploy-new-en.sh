961d50c05faf6baeccebbc80420be04df83c9b7d
maxgoldhouse <dklongley@maxgoldhouse.com>
2019-11-25 00:32:46 +0000

    [skip ci]

---
+++ deploy-new-en.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+date
+echo 'Fetching data from mgh-props'
+python getdata.py
+echo 'Data fetched'
+echo 'NEW ENGLISH'
+python ./scripts/NEW-en-scripts/topsix.py
+echo 'NEW topsix EN fin'
+#python ./scripts/NEW-en-scripts/latest.py
+#echo 'NEW latest EN fin'
+#python ./scripts/NEW-en-scripts/proplist.py
+#echo 'NEW proplist EN fin'
+#python scripts/NEW-en-scripts/propdetail.py
+#echo 'NEW propdetail EN fin'
+date
