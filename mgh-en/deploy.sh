67ae546cb4414418d289c3cd94fd18962f26b30e
dindinet <github.dklongley@neverbox.com>
2020-01-02 13:31:34 +0000

    no prop details for mgh-en

--- mgh-en/deploy.sh
+++ mgh-en/deploy.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 date
-echo 'www-mgh-3 deploy.sh Fetching data from mgh-props'
+echo 'mgh-en deploy.sh Fetching data from mgh-props'
 python getdata.py
 cp refsearch.html ./templates/NEW-en-templates/refsearch.html
 echo 'Data fetched'
 echo 'NEW ENGLISH'
 python ./scripts/NEW-en-scripts/topsix.py
 echo 'mgh-en deploy.sh topsix NEW-EN fin'
+python ./scripts/NEW-en-scripts/latest.py
+echo 'mgh-en deploy.sh latest NEW-EN fin'
+python ./scripts/NEW-en-scripts/proplist.py
+echo 'mgh-en deploy.sh proplist NEW-EN fin'
+python ./scripts/NEW-en-scripts/propdetail.py
+echo 'mgh-en deploy.sh propdetail NEW-EN fin'
 date
-#python ./scripts/NEW-en-scripts/latest.py
-#echo 'mgh-en deploy.sh latest NEW-EN fin'
-#python ./scripts/NEW-en-scripts/proplist.py
-#echo 'mgh-en deploy.sh proplist NEW-EN fin'
-#python ./scripts/NEW-en-scripts/propdetail.py
-#echo 'mgh-en deploy.sh propdetail NEW-EN fin'
-#date
