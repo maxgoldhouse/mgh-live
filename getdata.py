2938b6a34fc143ec6ea9eea2a23322e4a1e2acfb
dindinet <github.dklongley@neverbox.com>
2020-03-16 18:02:20 +0000

    rollback to pulling the data [skip ci]

--- getdata.py
+++ getdata.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import urllib
-#urllib.urlretrieve ("http://mgh-props.appspot.com/pydata", "_mgh_data.py")
+urllib.urlretrieve ("http://mgh-props.appspot.com/pydata", "_mgh_data.py")
 urllib.urlretrieve ("http://mgh-props.appspot.com/refs", "refsearch.html")
 urllib.urlretrieve ("http://mgh-props.appspot.com/getgroupedlocations","distinctlocations.html")
 #urllib.urlretrieve ("http://mgh-props.appspot.com/getdistinctlocations","distinctlocations.html")
