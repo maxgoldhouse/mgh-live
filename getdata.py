f0ce0f35176158d2c073125863f4c3ae35479b78
dindinet <github.dklongley@neverbox.com>
2020-03-17 12:18:41 +0000

    fetching the data from the bucket [skip ci]

--- getdata.py
+++ getdata.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python
 import urllib
-urllib.urlretrieve ("http://mgh-props.appspot.com/pydata", "_mgh_data.py")
-f = open('_mgh_data.py')
-line = f.readline()
-f.close()
-if line != '# -*- coding: utf-8 -*-\n':
-    urllib.urlretrieve ("http://mgh-props.appspot.com/pydata", "_mgh_data.py")
+
+### urllib.urlretrieve ("http://mgh-props.appspot.com/pydata", "_mgh_data.py")
+### f = open('_mgh_data.py')
+### line = f.readline()
+### f.close()
+### if line != '# -*- coding: utf-8 -*-\n':
+###     urllib.urlretrieve ("http://mgh-props.appspot.com/pydata", "_mgh_data.py")
+
+urllib.urlretrieve("https://storage.cloud.google.com/mgh-props.appspot.com/_mgh_data.py","_mgh_data.py")
     
 urllib.urlretrieve ("http://mgh-props.appspot.com/refs", "refsearch.html")
 urllib.urlretrieve ("http://mgh-props.appspot.com/getgroupedlocations","distinctlocations.html")
+
 #urllib.urlretrieve ("http://mgh-props.appspot.com/getdistinctlocations","distinctlocations.html")
 
 '''
