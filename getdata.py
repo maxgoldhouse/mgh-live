9c1d5336cbe6de8e725eacdc6d3dde0cc400fadd
dindinet <github.dklongley@neverbox.com>
2020-03-17 13:10:40 +0000

    get data from mgh-data folder in bucket [skip ci]

--- getdata.py
+++ getdata.py
@@ -8,7 +8,8 @@ import urllib
 ### if line != '# -*- coding: utf-8 -*-\n':
 ###     urllib.urlretrieve ("http://mgh-props.appspot.com/pydata", "_mgh_data.py")
 
-urllib.urlretrieve("https://storage.cloud.google.com/mgh-props.appspot.com/_mgh_data.py","_mgh_data.py")
+urllib.urlretrieve("https://storage.googleapis.com/mgh-props.appspot.com/mgh-data/_mgh_python.py","_mgh_data.py")
+
     
 urllib.urlretrieve ("http://mgh-props.appspot.com/refs", "refsearch.html")
 urllib.urlretrieve ("http://mgh-props.appspot.com/getgroupedlocations","distinctlocations.html")
