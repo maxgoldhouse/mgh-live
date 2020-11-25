2baac398c0b7ff171d8b58f694540df31c53741e
dindinet <github.dklongley@neverbox.com>
2020-11-25 12:13:36 +0000

    remove Clare contactsfrom templates[skip ci]

--- scripts/fr-scripts/fr_topsix.py
+++ scripts/fr-scripts/fr_topsix.py
@@ -85,7 +85,7 @@ for eachprop in _mgh_data.proplists['All']:
     prop['description'] =  ' '.join(row['FR'].split()[:50])
     prop['jsondescription'] = ' '.join(row['FR'].split()[:50]).decode('utf-8')
     chambre = ' chambres'
-	bain = ' salles de bains'
+    bain = ' salles de bains'
     if int(row['beds']) == 1:
 		chambre = ' chambre'
     elif int(row['beds']) > 1:
