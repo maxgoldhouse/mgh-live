a03b85d07d9277971ee7b3354cd9e983ceb5ce4b
dklongley <dklongley@gmail.com>
2021-05-18 16:00:16 +0100

    [skip ci] Put NEW- EN version back up

--- mgh-en/deploy.sh
+++ mgh-en/deploy.sh
@@ -5,14 +5,14 @@ python getdata.py
 cp refsearch.html ./templates/NEW-en-templates/refsearch.html
 cp distinctlocations.html ./templates/NEW-en-templates/distinctlocations.html
 echo 'Data fetched'
-echo 'NEW FRENCH'
-python ./scripts/NEW-fr-scripts/topsix.py
+echo 'NEW ENGLISH'
+python ./scripts/NEW-en-scripts/topsix.py
 echo 'mgh-en deploy.sh topsix NEW-EN fin'
-python ./scripts/NEW-fr-scripts/latest.py
+python ./scripts/NEW-en-scripts/latest.py
 echo 'mgh-en deploy.sh latest NEW-EN fin'
-python ./scripts/NEW-fr-scripts/proplist.py
+python ./scripts/NEW-en-scripts/proplist.py
 echo 'mgh-en deploy.sh proplist NEW-EN fin'
-python ./scripts/NEW-fr-scripts/propdetail.py
+python ./scripts/NEW-en-scripts/propdetail.py
 echo 'mgh-en deploy.sh propdetail NEW-EN fin'
 date
 echo 'finished'
