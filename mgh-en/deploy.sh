4299e6f4df7a2ae05e46ff14b5361638e3f668ca
dklongley <dklongley@gmail.com>
2021-05-25 23:04:43 +0100

    [skip ci] domains to templates

--- mgh-en/deploy.sh
+++ mgh-en/deploy.sh
@@ -2,18 +2,18 @@
 date
 echo 'mgh-en deploy.sh Fetching data from mgh-props'
 python getdata.py
-cp refsearch.html ./templates/NEW-nl-templates/refsearch.html
-cp distinctlocations.html ./templates/NEW-nl-templates/distinctlocations.html
-cat ./templates/NEW-nl-templates/distinctlocations.html
+cp refsearch.html ./templates/NEW-de-templates/refsearch.html
+cp distinctlocations.html ./templates/NEW-de-templates/distinctlocations.html
+cat ./templates/NEW-de-templates/distinctlocations.html
 echo 'Data fetched'
-echo 'NEW ENGLISH NEW-nl'
-python ./scripts/NEW-nl-scripts/topsix.py
-echo 'mgh-en deploy.sh topsix NEW-nl fin'
-python ./scripts/NEW-nl-scripts/latest.py
-echo 'mgh-en deploy.sh latest NEW-nl fin'
-python ./scripts/NEW-nl-scripts/proplist.py
-echo 'mgh-en deploy.sh proplist NEW-nl fin'
-python ./scripts/NEW-nl-scripts/propdetail.py
-echo 'mgh-en deploy.sh propdetail NEW-nl fin'
+echo 'NEW ENGLISH NEW-de'
+python ./scripts/NEW-de-scripts/topsix.py
+echo 'mgh-en deploy.sh topsix NEW-de fin'
+python ./scripts/NEW-de-scripts/latest.py
+echo 'mgh-en deploy.sh latest NEW-de fin'
+python ./scripts/NEW-de-scripts/proplist.py
+echo 'mgh-en deploy.sh proplist NEW-de fin'
+python ./scripts/NEW-de-scripts/propdetail.py
+echo 'mgh-en deploy.sh propdetail NEW-de fin'
 date
 echo 'finished'
