de8c4bedbef90b88bbe1be2cc1221bcb507d9567
dklongley <dklongley@gmail.com>
2021-05-25 15:07:07 +0100

    [skip ci]--version nl cloudbuild yaml

--- mgh-en/deploy.sh
+++ mgh-en/deploy.sh
@@ -2,18 +2,18 @@
 date
 echo 'mgh-en deploy.sh Fetching data from mgh-props'
 python getdata.py
-cp refsearch.html ./templates/NEW-fr-templates/refsearch.html
-cp distinctlocations.html ./templates/NEW-fr-templates/distinctlocations.html
-cat ./templates/NEW-fr-templates/distinctlocations.html
+cp refsearch.html ./templates/NEW-nl-templates/refsearch.html
+cp distinctlocations.html ./templates/NEW-nl-templates/distinctlocations.html
+cat ./templates/NEW-nl-templates/distinctlocations.html
 echo 'Data fetched'
-echo 'NEW ENGLISH NEW-fr'
-python ./scripts/NEW-fr-scripts/topsix.py
-echo 'mgh-en deploy.sh topsix NEW-fr fin'
-python ./scripts/NEW-fr-scripts/latest.py
-echo 'mgh-en deploy.sh latest NEW-fr fin'
-python ./scripts/NEW-fr-scripts/proplist.py
-echo 'mgh-en deploy.sh proplist NEW-fr fin'
-python ./scripts/NEW-fr-scripts/propdetail.py
-echo 'mgh-en deploy.sh propdetail NEW-fr fin'
+echo 'NEW ENGLISH NEW-nl'
+python ./scripts/NEW-nl-scripts/topsix.py
+echo 'mgh-en deploy.sh topsix NEW-nl fin'
+python ./scripts/NEW-nl-scripts/latest.py
+echo 'mgh-en deploy.sh latest NEW-nl fin'
+python ./scripts/NEW-nl-scripts/proplist.py
+echo 'mgh-en deploy.sh proplist NEW-nl fin'
+python ./scripts/NEW-nl-scripts/propdetail.py
+echo 'mgh-en deploy.sh propdetail NEW-nl fin'
 date
 echo 'finished'
