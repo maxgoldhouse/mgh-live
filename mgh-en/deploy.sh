afe4ed86f3850fd2ece45eaefdeab90cd71f390d
dklongley <dklongley@gmail.com>
2021-06-10 10:28:21 +0100

    [skip ci] qs passing for nl

--- mgh-en/deploy.sh
+++ mgh-en/deploy.sh
@@ -2,18 +2,18 @@
 date
 echo 'Fetching data from mgh-props'
 python getdata.py
-cp refsearch.html ./templates/NEW-de-templates/refsearch.html
-cp distinctlocations.html ./templates/NEW-de-templates/distinctlocations.html
-cat ./templates/NEW-de-templates/distinctlocations.html
+cp refsearch.html ./templates/NEW-nl-templates/refsearch.html
+cp distinctlocations.html ./templates/NEW-nl-templates/distinctlocations.html
+cat ./templates/NEW-nl-templates/distinctlocations.html
 echo 'Data fetched'
-echo 'NEW ENGLISH NEW-de'
-python ./scripts/NEW-de-scripts/topsix.py
-echo 'topsix NEW-de fin'
-python ./scripts/NEW-de-scripts/latest.py
-echo 'latest NEW-de fin'
-python ./scripts/NEW-de-scripts/proplist.py
-echo 'proplist NEW-de fin'
-python ./scripts/NEW-de-scripts/propdetail.py
-echo 'propdetail NEW-de fin'
+echo 'NEW ENGLISH NEW-nl'
+python ./scripts/NEW-nl-scripts/topsix.py
+echo 'topsix NEW-nl fin'
+python ./scripts/NEW-nl-scripts/latest.py
+echo 'latest NEW-nl fin'
+python ./scripts/NEW-nl-scripts/proplist.py
+echo 'proplist NEW-nl fin'
+python ./scripts/NEW-nl-scripts/propdetail.py
+echo 'propdetail NEW-nl fin'
 date
 echo 'finished'
