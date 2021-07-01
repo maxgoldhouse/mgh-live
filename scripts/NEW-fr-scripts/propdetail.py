52e4b2a7f1641fbcb1fad4171f422db3a0370a5e
dklongley <dklongley@gmail.com>
2021-07-01 15:36:35 +0100

    [skip ci] remove accent e replace from topsix.py

--- scripts/NEW-fr-scripts/propdetail.py
+++ scripts/NEW-fr-scripts/propdetail.py
@@ -56,9 +56,9 @@ for prop in _mgh_data.props:
 
 	propurl_de = str(thisprop['beds'])+'-Schlafzimmer-'+de_proptype.replace(' ','-')+'-in-'+thisprop['location'].replace(' ','-')+'-'+thisprop['pid']+'.html'
 	propurl_nl = str(thisprop['beds'])+'-slaapkamer-'+nl_proptype.replace(' ','-')+'-in-'+thisprop['location'].replace(' ','-')+'-'+thisprop['pid']+'.html'
-	propurl_fr = str(thisprop['beds'])+'-chambre-'+fr_proptype.replace(' ','-').replace('e','e')+'-à-'+thisprop['location'].replace(' ','-')+'-'+thisprop['pid']+'.html'
+	propurl_fr = str(thisprop['beds'])+'-chambre-'+fr_proptype.replace(' ','-').replace('ée','ee')+'-à-'+thisprop['location'].replace(' ','-')+'-'+thisprop['pid']+'.html'
 	propurl_en = str(thisprop['beds'])+'-bed-'+thisprop['ptype'].replace(' ','-')+'-in-'+thisprop['location'].replace(' ','-')+'-'+thisprop['pid']+'.html'
-	pagename = str(thisprop['beds'])+'-chambre-'+fr_proptype.replace(' ','-')+'-a-'+thisprop['location'].replace(' ','-')+'-'+thisprop['pid']
+	pagename = str(thisprop['beds'])+'-chambre-'+fr_proptype.replace(' ','-').replace('ée','ee')+'-a-'+thisprop['location'].replace(' ','-')+'-'+thisprop['pid']
 	if thisprop['pool'].lower() == 'yes':
 		pool = 'Oui'
 	elif thisprop['pool'].lower() == 'no':
