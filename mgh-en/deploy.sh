75085bdcd3352e3791b2b916b674a1050e833ee8
dklongley <dklongley@gmail.com>
2021-07-07 11:45:22 +0100

    [skip ci] quick fr deploy

--- mgh-en/deploy.sh
+++ mgh-en/deploy.sh
@@ -2,18 +2,18 @@
 date
 echo 'Fetching data from mgh-props'
 python getdata.py
-cp refsearch.html ./templates/NEW-en-templates/refsearch.html
-cp distinctlocations.html ./templates/NEW-en-templates/distinctlocations.html
-cat ./templates/NEW-en-templates/distinctlocations.html
+cp refsearch.html ./templates/NEW-fr-templates/refsearch.html
+cp distinctlocations.html ./templates/NEW-fr-templates/distinctlocations.html
+cat ./templates/NEW-fr-templates/distinctlocations.html
 echo 'Data fetched'
-echo 'NEW ENGLISH NEW-en'
-python ./scripts/NEW-en-scripts/topsix.py
-echo 'topsix NEW-en fin'
-python ./scripts/NEW-en-scripts/latest.py
-echo 'latest NEW-en fin'
-python ./scripts/NEW-en-scripts/proplist.py
-echo 'proplist NEW-en fin'
-python ./scripts/NEW-en-scripts/propdetail.py
-echo 'propdetail NEW-en fin'
+echo 'NEW ENGLISH NEW-fr'
+python ./scripts/NEW-fr-scripts/topsix.py
+echo 'topsix NEW-fr fin'
+python ./scripts/NEW-fr-scripts/latest.py
+echo 'latest NEW-fr fin'
+python ./scripts/NEW-fr-scripts/proplist.py
+echo 'proplist NEW-fr fin'
+python ./scripts/NEW-fr-scripts/propdetail.py
+echo 'propdetail NEW-fr fin'
 date
 echo 'finished'
